432c4af3fd4ca1c76ffb36734248f9bf286cfef4
CrazyBucket <1475743203@qq.com>
2023-05-19 00:19:12 +0800

    重抽逻辑&抽奖记录

--- bigTurntable/src/App.jsx
+++ bigTurntable/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState, useRef } from "react";
 import { LuckyWheel } from "@lucky-canvas/react";
 import bcImg from "./assets/bcImg.jpg";
+import Modal from "./components/Modal";
 import "./App.css";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [showRecord, setShowRecord] = useState(false);
+  const [showBtn, setShowBtn] = useState(true);
+  const [text, setText] = useState("");
+  const [btn, setBtn] = useState("重抽");
+  const [reward, setReward] = useState("周边奖");
+  const [history, setHistory] = useState([]);
   const [blocks] = useState([{ padding: "10px", background: "#b14c3a" }]);
   const prizes = [
     {
@@ -25,6 +33,7 @@ const App = () => {
       ],
     },
   ];
+
   const [buttons] = useState([
     { radius: "40%", background: "#f9f5ea" },
     { radius: "35%", background: "#edd994" },
@@ -42,11 +51,11 @@ const App = () => {
     { prize: "一等奖" },
     { prize: "二等奖" },
     { prize: "二等奖" },
-    { prize: "周边奖品" },
-    { prize: "周边奖品" },
-    { prize: "周边奖品" },
-    { prize: "周边奖品" },
-    { prize: "周边奖品" },
+    { prize: "周边奖" },
+    { prize: "周边奖" },
+    { prize: "周边奖" },
+    { prize: "周边奖" },
+    { prize: "周边奖" },
   ]);
 
   const myLucky = useRef();
@@ -57,7 +66,7 @@ const App = () => {
         const randomIndex = Math.floor(Math.random() * pool.length);
         const selectedPrize = pool[randomIndex];
         let index = 0;
-        if (selectedPrize.prize === "周边奖品") {
+        if (selectedPrize.prize === "周边奖") {
           index = 2;
         }
         if (selectedPrize.prize === "一等奖") {
@@ -71,13 +80,35 @@ const App = () => {
         setPool([...pool]);
       }, 2500);
     } else {
-      alert("本轮奖品已经全部抽完！");
+      setText("本轮奖品已经全部抽完！");
+      setShowBtn(false);
+      setShowModal(true);
     }
   };
 
   const handleEnd = prize => {
     let x = Math.floor(Math.random() * 100) + 1;
-    alert("恭喜" + x + "号抽到" + prize.fonts[0].text);
+    setShowModal(true);
+    setText("恭喜" + x + "号抽到" + prize.fonts[0].text);
+    setReward(prize.fonts[0].text);
+    setHistory([...history, { number: x, prize: prize.fonts[0].text }]);
+  };
+
+  const handleOptionalClick = () => {
+    let newPool = [...pool, { prize: reward }];
+    let newHistory = [...history];
+    newHistory.pop();
+    setHistory(newHistory);
+    setPool(newPool);
+  };
+
+  const handleRequiredClick = () => {
+    setShowRecord(false);
+    setShowModal(false);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
   };
 
   return (
@@ -95,6 +126,46 @@ const App = () => {
           onEnd={handleEnd}
         />
       </div>
+      {showModal &&
+        (showBtn ? (
+          <Modal
+            text={text}
+            optionalButton={{ text: btn, onClick: handleOptionalClick }}
+            requiredButton={{ text: "确定", onClick: handleRequiredClick }}
+            onClose={handleClose}
+          />
+        ) : (
+          <Modal
+            text={text}
+            requiredButton={{ text: "确定", onClick: handleRequiredClick }}
+            onClose={handleClose}
+          />
+        ))}
+      <div
+        className="record"
+        onClick={() => {
+          setShowRecord(true);
+        }}
+      >
+        抽奖记录
+      </div>
+      {showRecord && (
+        <Modal
+          text={
+            history.length === 0
+              ? "暂无抽奖记录"
+              : history.map((item, index) => {
+                  return (
+                    <div key={index} style={{ whiteSpace: "pre-wrap" }}>
+                      {item.number + "号          " + item.prize}
+                    </div>
+                  );
+                })
+          }
+          requiredButton={{ text: "确定", onClick: handleRequiredClick }}
+          onClose={handleClose}
+        />
+      )}
     </div>
   );
 };
---
+++ bigTurntable/src/components/Modal.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./Modal.css";
+
+const Modal = ({ text, optionalButton, requiredButton, onClose }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleOptionalClick = () => {
+    if (optionalButton.onClick) {
+      optionalButton.onClick();
+    }
+    setIsVisible(false);
+    onClose();
+  };
+
+  const handleRequiredClick = () => {
+    requiredButton.onClick();
+    setIsVisible(false);
+    onClose();
+  };
+
+  const handleBackdropClick = () => {
+    setIsVisible(false);
+    onClose();
+  };
+
+  return isVisible ? (
+    <div className="modal-wrapper">
+      <div className="modal-backdrop" onClick={handleBackdropClick}></div>
+      <div className="modal-box">
+        <div className="modal-text">{text}</div>
+        <div>
+          {optionalButton && (
+            <button className="modal-btn" onClick={handleOptionalClick}>
+              {optionalButton.text}
+            </button>
+          )}
+          <button className="modal-btn" onClick={handleRequiredClick}>
+            {requiredButton.text}
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Modal;
